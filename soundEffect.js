d08c155381bfd95acab45cd23d7272531fd46bc0
kongwv <116346840+kongwv@users.noreply.github.com>
2023-10-19 03:58:09 +0700

    Update soundEffect.js

--- soundEffect.js
+++ soundEffect.js
@@ -1,5 +1,19 @@
-function playStart() {
+function playSearchBTN() {
     var start = new Audio();
     start.src = "sounds/start.mp3";
     start.play();
+}
+
+var music = document.getElementById("music");
+var x = new Boolean;
+
+function playMusic() {
+    if (x) {
+        x = false;
+        music.play();
+    } else {
+        x = true;
+        music.pause();
+        music.currentTime = 0;
+    }
 }
