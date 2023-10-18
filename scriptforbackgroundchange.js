98469da54121d2af59e12c3ae6ee419b6f8fc29e
l3adl3oy <foxzyaim@hotmail.com>
2023-10-19 00:37:29 +0700

    aa

--- scriptforbackgroundchange.js
+++ scriptforbackgroundchange.js
@@ -1,5 +1,5 @@
 
-
+let btn3 = document.querySelector('#btn-3');
 let btn1 = document.querySelector('#btn-1');
 let btn2 = document.querySelector('#btn-2');
 
@@ -8,4 +8,7 @@ btn1.addEventListener('click', () =>{
 })
 btn2.addEventListener('click', () =>{
     document.body.style.backgroundImage = "url('views/arttt.jpg')";
+})
+btn3.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('views/image2.png')";
 })
