be0701bacb8a4a6bafcef2206262e403e2b2f44d
l3adl3oy <foxzyaim@hotmail.com>
2023-10-19 16:53:56 +0700

    กก

--- scriptforbackgroundchange.js
+++ scriptforbackgroundchange.js
@@ -4,10 +4,10 @@ let btn1 = document.querySelector('#btn-1');
 let btn2 = document.querySelector('#btn-2');
 
 btn1.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('views/24.jpg')";
+    document.body.style.backgroundImage = "url('images/24.jpg')";
 })
 btn2.addEventListener('click', () =>{
-    document.body.style.backgroundImage = "url('views/qq.jpg')";
+    document.body.style.backgroundImage = "url('images/qq.jpg')";
 })
 btn3.addEventListener('click', () =>{
     document.body.style.backgroundImage = "url('views/image2.png')";
